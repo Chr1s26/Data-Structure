962a41427bfac6310b013b40d4f1e29993d97142
Htet Kyaw Swar Linn <htetkyawswarlinn44@gmail.com>
2025-12-08 18:46:41 +0700

    solve max sum sequence with Kadane's algo

--- src/InterviewQuestions/MaxSumOfSubSequence.java
+++ src/InterviewQuestions/MaxSumOfSubSequence.java
@@ -1,60 +1,89 @@
 public class MaxSumOfSubSequence{
     public static void main(String[] args) {
         int[] arr = {-2,-3,4,-1,-2,1,5,-3};
-        // int[] arr = {-1};
-        System.out.print(maxSum(arr, 0, arr.length));
+        System.out.println(maxSumv1(arr, 0, arr.length));
+        System.out.println(maxSumv2(arr, 0, arr.length));
+        System.out.print(maxSumv3(arr, 0, arr.length));
     }
 
 
-    public static int maxSum(int[] arr, int start, int end){
-        int sum = arr[start];
-        int maxSum = arr[start];
-        int pointer = 0;
-        // sum = -2;
-        // i = 1, prev = -2, sum = -3, maxsum = -2;
-        // i = 2, prev = -3, sum = 1, Maxsum = 1;
-        // i = 3, prev = 4, sum = 1-1 = 0, Maxsum = 1;
+    public static int maxSumv3(int[] arr, int start, int end){
 
-        for(int i = 1; i < end; i++){
-            sum += arr[i];
-            if(sum < maxSum){
-                sum -= arr[i-pointer];
-                pointer++;
-            }else{
-                maxSum = sum;
-            }
+        long startTime = System.nanoTime();
+
+        int current = arr[0];
+        int maxSum = arr[0];
+
+        for(int i = 1; i < arr.length; i++){
+            current = Math.max(arr[i], current + arr[i]);
+            maxSum = Math.max(current, maxSum);
         }
+    
+        long endTime = System.nanoTime();
+
+        System.out.println("Time: " + (endTime - startTime) + " ns");
+    
         return maxSum;
     }
 
-    // public static int maxSum(int[] arr, int start, int end){
+    public static int maxSumv2(int[] arr, int start, int end){
 
-    //     int maxSum = 0;
+        long startTime = System.nanoTime();
 
-    //     if(arr.length <= 1 ) return maxSum;
+        int sum = 0;
+        int maxSum = arr[start];
+        int n = arr.length;
 
-    //     int sum = arr[0];
-    //     int prev = 0;
+        if (n == 0) return 0;
+        if (n == 1) return arr[0];
+
+
+        int[] prefix = new int[n];
+        prefix[0] = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+
         
+        for(int i = 0; i < end; i++){
+            for(int j = i; j < end; j++){
+                if(i == 0) sum = prefix[j];
+                else sum = prefix[j] - prefix[i-1];
+                maxSum = Math.max(sum, maxSum);
+            }
+        }
 
-    //     for(int i = 1; i < end; i++){
-    //         prev = sum;
-    //         sum += arr[i];
-    //         maxSum = Math.max(prev, sum);
-    //         sum = maxSum;
-    //     }
-    //     return maxSum;
+        long endTime = System.nanoTime();
 
-    // }
+        System.out.println("Time: " + (endTime - startTime) + " ns");
+    
+        return maxSum;
+
+    }
+
+
+    public static int maxSumv1(int[] arr, int start, int end){
+
+        long startTime = System.nanoTime();
+        
+        if(arr.length == 1) return arr[0];
+
+        int sum = 0;
+        int maxSum = arr[start];
+        
+        for(int i = 0; i < end-1; i++){
+            sum = arr[i];
+            for(int j = i+1; j < end; j++){
+                sum += arr[j];
+                maxSum = Math.max(sum, maxSum);
+            }
+        }
+
+        long endTime = System.nanoTime();
+
+        System.out.println("Time: " + (endTime - startTime) + " ns");
+        return maxSum;
+    }
 }
 
-// maxSum = -2
-// sum = -2 + -3 = -5
-//maxSum = -2
-//sum = -2 + 4 = 2
-// maxSum = 2
-//-1, -2, 1
-//sum = 2 + 1 = 3
-//maxSum = 3
-//sum = 3 + 5 = 8
-//maxSum = 8
