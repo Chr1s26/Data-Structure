f634b01f06a23c7048036fdbc3cb03abaef547f7
Htet Kyaw Swar Linn <htetkyawswarlinn44@gmail.com>
2025-12-24 22:22:32 +0700

    Learn Graph Topo Sort

--- src/Practice/graphPractice.java
+++ src/Practice/graphPractice.java
@@ -49,8 +49,74 @@ public class graphPractice{
         return count;
     }
 
+    //Directed Graph
     public static boolean hasCycle(int node,int parent,boolean[] visited,List<List<Integer>> graph){
-        
-        return true;
+        visited[node] = true;
+        for(int nei : graph.get(node)){
+            if(!visited[nei]){
+                if(hasCycle(nei,node,visited,graph)){
+                    return true;
+                }
+            }else if(nei != parent){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean hasCycle(int node, boolean[] visited, boolean[] inStack,List<List<Integer>> graph) {
+        visited[node] = true;
+        inStack[node] = true;
+
+        for(int nei : graph.get(node)){
+            if(!visited[nei]){
+                if(hasCycle(nei,visited,inStack,graph)){
+                    return true;
+                }
+            }else if(inStack[nei]){
+                return true;
+            }
+        }
+        inStack[node] = false; //important
+        return false;
+    }
+
+    public static int[] topoSort(int n, List<List<Integer>> graph) {
+
+        int[] inDegree = new int[n];
+
+        for(int i = 0; i < n; i++){
+            for(int nei : graph.get(i)){
+                inDegree[nei]++;
+            }
+        }
+
+        Queue<Integer> q = new ArrayDeque<>();
+        for(int i = 0; i < n; i++){
+            if(inDegree[i] == 0){
+                q.offer(i);
+            }
+        }
+
+        int[] order = new int[n];
+        int idx = 0;
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+            order[idx++] = node;
+
+            for(int nei : graph.get(node)){
+                if(--inDegree[nei] == 0){
+                    q.offer(nei);
+                }
+            } 
+        }
+        return order;
     }
 }
+
+// 0 → 1
+// 0 → 2
+// Node 0:
+// indegree = 0
+// outdegree = 2
