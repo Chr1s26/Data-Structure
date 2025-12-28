bd59afb1573d883181085fd84e39efffb3378b65
Htet Kyaw Swar Linn <htetkyawswarlinn44@gmail.com>
2025-12-28 17:40:25 +0700

    Solve leet code problem Reverse String with Recursive

---
+++ src/Recursive/ReverseString.java
@@ -0,0 +1,20 @@
+public class ReverseString{
+    public static void main(String[] args){
+
+    }
+
+    public static void reverseString(char[] s) {
+        reverse(s,0,s.length - 1);
+    }
+
+    public static void reverse(char[] s, int left, int right){
+        if(left >= right) return;
+
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+
+
+        reverse(s,++left,--right);
+    }
+}
