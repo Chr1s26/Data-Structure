00efc33f44f856afce43dde819690eb227b64624
Htet Kyaw Swar Linn <htetkyawswarlinn44@gmail.com>
2026-01-23 21:36:12 +0700

    Solve Search in Rotated Sorted Array II

---
+++ src/BinarySearch/RotatedSortedArrayBinarySearch/SearchInRotatedSortedArrayII.java
@@ -0,0 +1,31 @@
+public class SearchInRotatedSortedArrayII{
+    public boolean search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+
+            if(nums[mid] == target) return true;
+
+            if(nums[left] == nums[mid] && nums[right] == nums[mid]){
+                left = left + 1;
+                right = right - 1;
+            }else if(nums[left] > nums[mid]){
+                if(nums[mid] < target && nums[right] >= target){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
+                }
+            }else if (nums[left] <= nums[mid]){
+                if(nums[left] <= target && nums[mid] > target){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
