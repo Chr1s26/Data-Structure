a6ea7637f6de4e14feeee8dfb2ff229cf6de3329
Htet Kyaw Swar Linn <htetkyawswarlinn44@gmail.com>
2026-01-27 12:40:58 +0700

    Practice

---
+++ src/NeetCodeExercise/TwoPointer/ContainerWithMostWater.java
@@ -0,0 +1,18 @@
+public class ContainerWithMostWater{
+    public int maxArea(int[] heights) {
+        int left = 0;
+        int right = heights.length - 1;
+        int max = 0;
+        while(left < right){
+            int area = Math.min(heights[left],heights[right]) * (right - left);
+            max = Math.max(max,area);
+            if(heights[left] > heights[right]){
+                right--;
+            }else{
+                left++;
+            }
+        }
+
+        return max;
+    }
+}
