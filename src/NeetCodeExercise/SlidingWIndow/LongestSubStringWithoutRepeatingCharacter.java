314f44cde991cc42f9f864126a6880d09f48b159
Htet Kyaw Swar Linn <htetkyawswarlinn44@gmail.com>
2026-01-27 14:20:45 +0700

    Practice

---
+++ src/NeetCodeExercise/SlidingWIndow/LongestSubStringWithoutRepeatingCharacter.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class LongestSubStringWithoutRepeatingCharacter{
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int max = 0;
+        int left = 0;
+        for(int i = 0; i < s.length(); i++){
+
+            while(set.contains(s.charAt(i))){
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(s.charAt(i));
+            max = Math.max(max,set.size());
+        }
+        return max;
+    }
+}
